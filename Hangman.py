7f47e65098fe1d200b5c842e6f269a0604266e90
Marcos Fabian <fabiancm@oregonstate.edu>
2023-08-01 19:54:12 -0700

    Initial commit

---
+++ Hangman.py
@@ -0,0 +1,14 @@
+word_list = [rule, didactic, gate, selective, coach, fat, enchanting, tiger, hand, damp, lame, table, education, teeny,
+             impolite, gather, unit, talk, disgusted, bat, teaching, circle, aback, scribble, attempt, afford, veil,
+             snakes, berry, fuzzy, guess, needy, poised, nut, open, flight, remind, bore]
+
+
+def draw_stand():
+def select_word():
+def guess_word():
+def play_game():
+def draw_part():
+def end_game():
+def generate_blank_spaces():
+def replace_with_letter():
+def
